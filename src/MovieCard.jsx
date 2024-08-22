7a1584863649cf6913a3f28125bd147431e88606
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-22 19:09:22 -0400

    feat: add content to MovieCard component

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,8 +1,31 @@
 export default function MovieCard({movie}) {
-    console.log(movie)
+
+    const posterUrl = movie.Poster !== "N/A" ? movie.Poster : "./assets/no-image-placeholder.png"
+
     return (
-        <div>
-            <h1>{movie.Title}</h1>
-        </div>
+        <>
+            <div>
+                <img src={posterUrl} alt="Movie Poster" />
+                <div className="movie-info-container">
+                    <div className="movie-info-one">
+                        <h2 className="movie-title">{movie.Title}</h2>
+                        <p className="movie-imdbRating">
+                            <i className="fa-solid fa-star star-icon"></i> {movie.imdbRating}
+                        </p>
+                    </div>
+                    <div className="movie-info-two">
+                        <p className="movie-runtime">
+                            <i className="fa-regular fa-clock"></i> {movie.Runtime}
+                        </p>
+                        <p className="movie-genre">
+                            <i className="fa-solid fa-video"></i> {movie.Genre}
+                        </p>
+                        <button>button</button>
+                    </div>
+                    <p className="movie-plot">{movie.Plot}</p>
+                </div>
+            </div>
+            <hr />
+        </>
     )
 }
