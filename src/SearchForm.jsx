2526566ec3f532a79e83c685782788fa11cba7d6
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-22 16:12:16 -0400

    feat: add handleChange function to input element in searchForm component

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -5,13 +5,18 @@ export default function searchForm() {
     
     const [inputValue, setInputValue] = useState('')
 
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
     return (
         <form>
-            <i class="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass"></i>
             <input 
                 type="search" 
                 placeholder="Search for a movie"
                 value={inputValue}
+                onChange={handleChange}
             />
             <button type="submit">Search</button>
         </form>
