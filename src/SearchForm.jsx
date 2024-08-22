e598a2d6e182b88670071833762a08ba3042adb8
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-22 15:53:34 -0400

    feat: add inputValue state in searchForm component

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import "./SearchForm.css"
 
 export default function searchForm() {
+    
+    const [inputValue, setInputValue] = useState('')
+
     return (
         <form>
             <i class="fa-solid fa-magnifying-glass"></i>
             <input 
                 type="search" 
                 placeholder="Search for a movie"
+                value={inputValue}
             />
             <button type="submit">Search</button>
         </form>
