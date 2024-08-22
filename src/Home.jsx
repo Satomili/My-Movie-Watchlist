e4f9824bbc6d63f0ec1408c29f109d8715897a0b
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-22 15:46:33 -0400

    feat: add movies state in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header"
 import SearchForm from "./SearchForm"
 import MovieList from "./MovieList"
+import { useState } from "react"
 
 export default function Home() {
+    const [movies, setMovies] = useState([])
+
+
     return (
         <div>
             <Header />
