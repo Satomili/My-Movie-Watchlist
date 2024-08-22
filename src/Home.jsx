a76f30da1de4fc2fadc828c234a5348311857bc4
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-22 15:44:10 -0400

    feat: add basic content to MovieList component and import it in Home and Watchlist components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import SearchForm from "./SearchForm"
+import MovieList from "./MovieList"
 
 export default function Home() {
     return (
         <div>
             <Header />
             <SearchForm />
+            <MovieList />
         </div>
     )
 }
--- src/Watchlist.jsx
+++ src/Watchlist.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header"
+import MovieList from "./MovieList"
 
 export default function Watchlist() {
     return (
         <div>
             <Header />
+            <MovieList />
         </div>
     )
 }
