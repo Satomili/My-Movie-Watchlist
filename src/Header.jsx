41e9094103e55ae3c4d4d8040c7e66109b6092e1
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-23 15:53:57 -0400

    refactor: display different Header content based on the current route

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Header.css"
 
 export default function Header() {
+    const location = useLocation()
+
+    const isHomePage = location.pathname === "/"
+    const isWatchlistPage = location.pathname === "/watchlist"
+
     return (
         <header>
-            <h1>
-                <a href="/">Find your film</a>
-            </h1>
-            <p>
-                <a href="/watchlist">
-                    <i className="fa-solid fa-list"></i> My Watchlist
-                </a>
-            </p>
+            {isHomePage && (
+                <>
+                    <h1>
+                        <Link to="/">Find your film</Link>
+                    </h1>
+                    <p>
+                        <Link to="/watchlist">
+                            <i className="fa-solid fa-list"></i> My Watchlist
+                        </Link>
+                    </p>
+                </>
+            )}
+            {isWatchlistPage && (
+                <>
+                    <h1>
+                        <Link to="/">My Watchlist</Link>
+                    </h1>
+                    <p>
+                        <Link to="/">
+                            <i className="fa-solid fa-magnifying-glass"></i> Search for movies
+                        </Link>
+                    </p>
+                </>
+            )}
         </header>
     )
 }
