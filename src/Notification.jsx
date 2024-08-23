dc0d34e956302aa8a32fe19d3e4c27e3ac5d59b5
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-23 13:20:03 -0400

    feat: add styles to Notification component in Notification.css

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,10 +1,14 @@
+import './Notification.css'
+
 export default function Notification({message, isShow, isInWatchlist}) {
     return (
-        <div className={`notification ${isShow ? 'show' : ''}`} >
-            <i className="fa-solid fa-check"></i>
-            <div className="notification-message">
-                <h3>{message}</h3>
-                {!isInWatchlist && <p>check your movielist!</p> }
+        <div className={`notification-container ${isShow ? 'show' : ''}`}>
+            <div className="notification" >
+                <i className="fa-solid fa-check"></i>
+                <div className="notification-message">
+                    <h3>{message}</h3>
+                    {!isInWatchlist && <p>Check your watchlist from <b><a href="/watchlist">here</a></b></p>}
+                </div>
             </div>
         </div>
     )
