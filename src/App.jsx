fc53a55af9c9ce290d686d30c510a083b12e7826
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-23 11:53:00 -0400

    feat: add useEffect in App.jsx to update localStorage when myWatchlist changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Watchlist from './Watchlist'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
 
   const [myWatchlist, setMyWatchlist] = useState(() => {
-    return JSON.parse(localStorage.getItem("myWatchlist")) || []
+    return JSON.parse(localStorage.getItem("myMovieWatchlist")) || []
   })
+
+  useEffect(() => {
+    localStorage.setItem('myWatchlist', JSON.stringify(myWatchlist))
+  }, [myWatchlist])
 
   const addToMyWatchlist = (movie) => {
     if (!myWatchlist.find(({ imdbID }) => imdbID === movie.imdbID )) {
