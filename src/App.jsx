dbb5b1b25e69c350d62fcfbd2304f193a608dc2d
Satomi Li <126296454+Satomili@users.noreply.github.com>
2024-08-23 10:50:51 -0400

    feat: add React state myWatchlist to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Watchlist from './Watchlist'
+import { useState } from 'react'
 
 
 function App() {
+
+  const [myWatchlist, setMyWatchlist] = useState([])
 
   return (
     <Router>
